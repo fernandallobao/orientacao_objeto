2d11e64e8120b3c6901213e4c907e98bd8173804
fernandallobao <fernandafcclobao@gmail.com>
2024-09-09 17:26:59 -0300

    atualizando

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 # NOTE: cria a classe
 class Pessoa:
     #os atributos são sempre definidos dentro do método constrtutor
@@ -21,14 +23,17 @@ class Pessoa:
             
 if __name__ == '__main__':
     #entrada de dados
+    humores = (True, False)
     nome1 = input('Informe o nome da primeira pessoa: ')
     nome2 = input('Informe o nome da segunda pessoa: ')
 
     #instanciando o objeto
-    usuario1 = Pessoa(nome2, humor=True)
-    usuario2 = Pessoa(nome1, humor=True)
+    usuario1 = Pessoa(nome2, humores[random.randit(0,1)]) #soteia e adiciona o true e false
+    usuario2 = Pessoa(nome1, humores[random.randit(0,1)])
 
+
+    
     #saida de dados
-    print(usuario1.cumprimentar(nome1,nome2, humor=True))
-    print(usuario2.resposta_cumprimentar(nome1, nome2, humor=True))                                                                       
+    print(usuario1.cumprimentar(nome1,nome2 )
+    print(usuario2.resposta_cumprimentar(nome1, nome2)                                                                       
     
